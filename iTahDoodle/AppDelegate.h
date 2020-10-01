27bb699c0e2362ad954a40bfe41da79514735efe
Nick <ptnguyen1901@gmail.com>
2020-10-01 10:37:59 -0400

    setup

--- iTahDoodle/AppDelegate.h
+++ iTahDoodle/AppDelegate.h
@@ -9,6 +9,13 @@
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
+@property (nonatomic) UITableView *taskTable;
+@property (nonatomic) UITextField *taskField;
+@property (nonatomic) UIButton *insertButton;
+
+@property (nonatomic) NSMutableArray *tasks;
+
+- (void)addTasks:(id)sender;
 
 @end
 
